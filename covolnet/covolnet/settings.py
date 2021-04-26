17acaebc9d774654d9b815ca9520efa57dd80e62
Shemin Anto <13395437+sheminanto@users.noreply.github.com>
2021-04-26 23:52:19 +0530

    debug=true

--- covolnet/covolnet/settings.py
+++ covolnet/covolnet/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w3_czq*-0=2pqd%92%u9k)mr$t$=5rt2w6e31^^a#_66y0k@9k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
